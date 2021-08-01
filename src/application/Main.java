31c09f0a0022d1d996a045e9b4bfcd4a86a69102
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-07-31 21:05:48 -0300

    Testando o fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,28 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start (Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+		AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+		Scene scene = new Scene(anchorPane);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		}
+		catch(IOException e) {
 			e.printStackTrace();
 		}
-	}
+		
+		}
 	
 	public static void main(String[] args) {
 		launch(args);
