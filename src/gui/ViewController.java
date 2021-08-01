a08168f5ab36169eb266a6353074b7522ef5619a
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-07-31 21:40:42 -0300

    Testando Eventos com javafx

---
+++ src/gui/ViewController.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+
+	@FXML
+	private Button button;
+	
+	@FXML
+	public void onButtonAction() {
+		System.out.println("Olá, mundo!");
+	}
+	
+}
